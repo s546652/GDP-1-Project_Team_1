891c860521f8c16e93e3022984accb153a70a5fd
naveen007kumar <30547716+naveen007kumar@users.noreply.github.com>
2022-09-23 14:33:51 -0500

    Developed first stage of Login Page
    Added two text fields to enter SID and passwords and also a submit button.

---
+++ EventsUpLogin/EventsUpLogin/EventsUpLogin/ContentView.swift
@@ -0,0 +1,86 @@
+//
+//  ContentView.swift
+//  EventsUpLogin
+//
+//  Created by Gundu,Naveen Kumar on 9/23/22.
+//
+
+import SwiftUI
+
+
+
+struct ContentView: View {
+    
+    @State private var sid = ""
+    @State private var password = ""
+    @State private var wrongSid = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+
+    var body: some View {
+        NavigationView{
+                   ZStack{
+                       Color.white
+                           .ignoresSafeArea()
+                       Circle()
+                           .scale(1.7)
+                           .foregroundColor(.white.opacity(0.15))
+                       Circle()
+                           .scale(1.35)
+                           .foregroundColor(.white)
+                       
+                       VStack {
+                           Text("Login")
+                               .font(.largeTitle)
+                               .bold()
+                               .padding()
+                           TextField("SID", text: $sid)
+                               .padding()
+                               .frame(width: 300, height: 50)
+                               .background(Color.black.opacity(0.05))
+                               .cornerRadius(10)
+                               .border(.red, width: CGFloat(wrongSid))
+                           
+                           SecureField("Password", text: $password)
+                               .padding()
+                               .frame(width: 300, height: 50)
+                               .background(Color.black.opacity(0.05))
+                               .cornerRadius(10)
+                               .border(.red, width: CGFloat(wrongPassword))
+                           
+                           Button("Submit"){
+                               authenticateUser(sid: sid, password: password)
+                           }
+                           .foregroundColor(.white)
+                           .frame(width: 150, height: 50)
+                           .background(Color.red)
+                           .cornerRadius(25)
+                           
+                           
+                       }
+                   }
+                   .navigationBarHidden(true)
+               }
+               
+           }
+           func authenticateUser(sid: String, password: String) {
+               if sid.lowercased() == "s546960"{
+                   wrongSid = 0
+                   if password.lowercased() == "abc123"{
+                       wrongPassword = 0
+                       showingLoginScreen = true
+                   } else {
+                       wrongPassword = 2
+                   }
+               } else {
+                   wrongSid = 2
+               }
+           }
+       }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EventsUpLogin/EventsUpLogin/EventsUpLogin/EventsUpLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventsUpLoginApp.swift
+//  EventsUpLogin
+//
+//  Created by Gundu,Naveen Kumar on 9/23/22.
+//
+
+import SwiftUI
+
+@main
+struct EventsUpLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
