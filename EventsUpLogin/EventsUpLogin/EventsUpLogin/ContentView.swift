3aa6b3956e93377f5ef0cfad954ec993529af4cc
naveen007kumar <30547716+naveen007kumar@users.noreply.github.com>
2022-09-23 14:43:09 -0500

    Added a background color.
    Added some interesting background to the login page.

--- EventsUpLogin/EventsUpLogin/EventsUpLogin/ContentView.swift
+++ EventsUpLogin/EventsUpLogin/EventsUpLogin/ContentView.swift
@@ -20,7 +20,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView{
                    ZStack{
-                       Color.white
+                       Color.red
                            .ignoresSafeArea()
                        Circle()
                            .scale(1.7)
