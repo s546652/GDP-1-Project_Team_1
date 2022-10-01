cd5938eb925a8d5bb79cd3ef90852354859498d4
naveen007kumar <30547716+naveen007kumar@users.noreply.github.com>
2022-09-30 19:49:53 -0500

    Added back buttons
    Added back buttons in Event List and Login page and included its functionality

--- EventsUp/EventsUp/ViewController.swift
+++ EventsUp/EventsUp/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pageView: UIPageControl!
+    @IBOutlet weak var slideCollectionView: UICollectionView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
