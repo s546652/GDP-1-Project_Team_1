c487e1dfa071d832d47feaf396875b2d374be6b6
s546652 <98067009+s546652@users.noreply.github.com>
2022-10-02 21:43:25 -0500

    Added new page for Event Details
    Added new page called event details and all required navigations are provided including back button.

---
+++ EventsUp/EventsUp/EventsListController.swift
@@ -0,0 +1,52 @@
+//
+//  EventsListController.swift
+//  EventsUp
+//
+//  Created by Gagenapally,Roshni Damodar Reddy on 10/2/22.
+//
+
+import UIKit
+
+class EventsListController: UIViewController {
+    
+    @IBOutlet weak var dateOutlet: UILabel!
+    
+    
+    @IBOutlet weak var eventOneOutlet: UIButton!
+    
+    @IBOutlet weak var eventTwoOutlet: UIButton!
+    
+    @IBOutlet weak var eventThreeOutlet: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func eventOneButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func eventTwoButton(_ sender: Any) {
+    }
+    
+    @IBAction func eventThreeButton(_ sender: Any) {
+    }
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- EventsUp/EventsUp/ViewController.swift
+++ EventsUp/EventsUp/ViewController.swift
@@ -9,14 +9,24 @@ import UIKit
 import SwiftUI
 
 class ViewController: UIViewController {
+     
+    var imgNum = 0
     
+    @IBOutlet weak var displayImageOutlet: UIImageView!
+    let img = ["img1","img2","img3"]
     
+    @IBOutlet weak var preOutlet: UIButton!
+    
+    @IBOutlet weak var nextOutlet: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        displayImageOutlet.image = UIImage(named: img[imgNum])
+        preOutlet.isEnabled = false
+
     }
-
-
+     
+    
 }
 
