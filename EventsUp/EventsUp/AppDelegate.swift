64a1f8b29b276f8b181493f2c8b4a35310e06a45
Rajitha Bandaru <48714015+Rajitha9595@users.noreply.github.com>
2022-10-13 19:07:34 -0500

    Code to create and connect to Database

--- EventsUp/EventsUp/AppDelegate.swift
+++ EventsUp/EventsUp/AppDelegate.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import SQLite3
+
+var dbURL = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -29,6 +32,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func createAndOpenDatabase() -> OpaquePointer? {
+        var db: OpaquePointer?
+        
+        let url = NSURL(fileURLWithPath: dbURL)
+        
+        if let pathComponenent = url.appendingPathComponent("EventsUp.sqlite")
+        {
+            let filePath = pathComponenent.path
+            if sqlite3_open(filePath, &db) == SQLITE_OK
+            {
+                print("Succesfully opened the Database at \(filePath)")
+                
+                return db;
+            }
+            else{
+                
+                print("Could not open the Database")
+                
+            }
+            
+            
+        }
+        else{
+            
+            print("File path is not available")
+        }
+        
+        return db;
     }
 
 
