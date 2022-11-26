f6972aba94250caf96e68db510480a2bc4adf016
naveen007kumar <30547716+naveen007kumar@users.noreply.github.com>
2022-11-25 20:00:44 -0600

    Added animations and other features for Signup page
    Added few animations and color change functionality to the signup page

--- EventsUp/EventsUp/signupController.swift
+++ EventsUp/EventsUp/signupController.swift
@@ -23,10 +23,10 @@ class signupControllerViewController: UIViewController {
     
     
     @IBOutlet weak var email: UITextField!
-
+    
     
     @IBOutlet weak var dob: UITextField!
-
+    
     @IBOutlet weak var sid: UITextField!
     
     @IBOutlet weak var phNo: UITextField!
@@ -40,7 +40,7 @@ class signupControllerViewController: UIViewController {
     override func viewDidLoad() {
         print("akjbvak")
         super.viewDidLoad()
-
+        
         self.fName.layer.borderColor = UIColor.red.cgColor
         self.fName.layer.borderWidth = 1
         
@@ -62,19 +62,19 @@ class signupControllerViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    
     @IBAction func Test(_ sender: UIButton) {
         print("testttt")
     }
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
     @IBOutlet weak var emailError: UILabel!
     
     var stdtAcc = false
@@ -145,11 +145,11 @@ class signupControllerViewController: UIViewController {
     
     
     @IBAction func toolTip(_ sender: Any) {
-     
+        
     }
     
     @IBAction func Submit(_ sender: Any) {
-    
+        
         if(fName.text!.isEmpty || lName.text!.isEmpty || email.text!.isEmpty || dob.text!.isEmpty || phNo.text!.isEmpty || password.text!.isEmpty || re_enterPassword.text!.isEmpty){
             if(fName.text!.isEmpty){
                 fName.shake()
@@ -193,92 +193,93 @@ class signupControllerViewController: UIViewController {
             }
             if(phNo.text!.isEmpty == false){
                 var phno:Int
-              //  phno = (Int)phNo.text!
+                //  phno = (Int)phNo.text!
             }
             var studentid:String!
             if(sid.text!.isEmpty == false){
                 studentid = sid.text
             }
-               let con = MySQL.Connection()
-        let db_name = "EventsUp"
-        
-        do{
-          // open a new connection
-            try
-            con.open("localhost", user: "root", passwd: "Skr@329532")
-            print("Connection is succesfull")
-            try con.use("EventsUp")
-            let ins_stmt = try con.prepare("INSERT INTO ATTENDEE(Password, SID, LName, FName,EmailId,PhoneNumber,DOB) VALUES(?,?,?,?,?,?,?)")
+            let con = MySQL.Connection()
+            let db_name = "EventsUp"
+            
+            do{
+                // open a new connection
+                try
+                con.open("localhost", user: "root", passwd: "Skr@329532")
+                print("Connection is succesfull")
+                try con.use("EventsUp")
+                let ins_stmt = try con.prepare("INSERT INTO ATTENDEE(Password, SID, LName, FName,EmailId,PhoneNumber,DOB) VALUES(?,?,?,?,?,?,?)")
                 try ins_stmt.exec([password.text!,"s546652", lName.text!, fName.text!, email.text!, phNo.text!, dob.text!])
-      }
-     catch (let e) {
-         print("Error Message")
-          print(e)
-    }
+            }
+            catch (let e) {
+                print("Error Message")
+                print(e)
+            }
             
             
-//             var insert = "INSERT INTO E_ATTENDEES (Attendee_Id,Password,Name,FName,EmailId,PhoneNUmber,DOB) VALUES (002,?, ?, ?, ?, ?, ?);"
-//             var insertStat : OpaquePointer?
-//             print("insert 1")
-//             if(sqlite3_prepare_v2(dbQueue, insert, -1, &insertStat, nil)) == SQLITE_OK {
-//                 sqlite3_bind_text(insertStat, 1, password.text ?? "", -1,sqlite_TRANSIENT)
-//                 sqlite3_bind_text(insertStat, 2, lName.text ?? "", -1,sqlite_TRANSIENT)
-//                 sqlite3_bind_text(insertStat, 3, fName.text ?? "", -1,sqlite_TRANSIENT)
-//                 sqlite3_bind_text(insertStat, 4, email.text ?? "", -1,sqlite_TRANSIENT)
-//                 sqlite3_bind_text(insertStat, 5, phNo.text ?? "", -1,sqlite_TRANSIENT)
-//                 sqlite3_bind_text(insertStat, 6, dob.text ?? "", -1,sqlite_TRANSIENT)
-//                 print("insert2")
-                
-//                 if(sqlite3_step(insertStat)) == SQLITE_DONE{
-                    
-//                     lName.text = "testing Roshni"
-                    
-//                     lName.becomeFirstResponder()
-//                     print("Success")
-                    
-//                 }
-//                 else{
-//                     print("error in data to table")
-//                 }
-                
-                
-//                 sqlite3_finalize(insertStat)
-//             }
+            //             var insert = "INSERT INTO E_ATTENDEES (Attendee_Id,Password,Name,FName,EmailId,PhoneNUmber,DOB) VALUES (002,?, ?, ?, ?, ?, ?);"
+            //             var insertStat : OpaquePointer?
+            //             print("insert 1")
+            //             if(sqlite3_prepare_v2(dbQueue, insert, -1, &insertStat, nil)) == SQLITE_OK {
+            //                 sqlite3_bind_text(insertStat, 1, password.text ?? "", -1,sqlite_TRANSIENT)
+            //                 sqlite3_bind_text(insertStat, 2, lName.text ?? "", -1,sqlite_TRANSIENT)
+            //                 sqlite3_bind_text(insertStat, 3, fName.text ?? "", -1,sqlite_TRANSIENT)
+            //                 sqlite3_bind_text(insertStat, 4, email.text ?? "", -1,sqlite_TRANSIENT)
+            //                 sqlite3_bind_text(insertStat, 5, phNo.text ?? "", -1,sqlite_TRANSIENT)
+            //                 sqlite3_bind_text(insertStat, 6, dob.text ?? "", -1,sqlite_TRANSIENT)
+            //                 print("insert2")
+            
+            //                 if(sqlite3_step(insertStat)) == SQLITE_DONE{
+            
+            //                     lName.text = "testing Roshni"
+            
+            //                     lName.becomeFirstResponder()
+            //                     print("Success")
+            
+            //                 }
+            //                 else{
+            //                     print("error in data to table")
+            //                 }
             
             
-//             let selectStatementString = "SELECT Attendee_Id,Password,Name,FName,EmailId,PhoneNUmber,DOB FROM E_ATTENDEES"
+            //                 sqlite3_finalize(insertStat)
+            //             }
             
-//             let tablecount = "SELECT COUNT FROM E_ATTENDEES"
             
-//             var selectStatemetnQuery :  OpaquePointer?
+            //             let selectStatementString = "SELECT Attendee_Id,Password,Name,FName,EmailId,PhoneNUmber,DOB FROM E_ATTENDEES"
             
-//             var sShowData = ""
+            //             let tablecount = "SELECT COUNT FROM E_ATTENDEES"
             
-//            if sqlite3_prepare_v2(dbQueue, selectStatementString, -1, &selectStatemetnQuery, nil) == SQLITE_OK
-//             {
-//                 while sqlite3_step(selectStatemetnQuery) == SQLITE_ROW {
-                    
-//                     sShowData += String(cString: sqlite3_column_text(selectStatemetnQuery, 0)) + "\t\t" +
-//                                  String(cString: sqlite3_column_text(selectStatemetnQuery, 1)) + "\t\t" +
-//                                  String(cString: sqlite3_column_text(selectStatemetnQuery, 2)) + "\t\t" +
-//                                 String(cString: sqlite3_column_text(selectStatemetnQuery, 3)) + "\t\t" +
-//                                 String(cString: sqlite3_column_text(selectStatemetnQuery, 4)) + "\t\t" +
-//                                 String(cString: sqlite3_column_text(selectStatemetnQuery, 5)) + "\n"
-//                     }
-               
-//              //  sShowData += String(cString: sqlit)
-                
-//                 sqlite3_finalize(selectStatemetnQuery)
-//             }
-//             print("selecting data")
-//             print(sShowData)
+            //             var selectStatemetnQuery :  OpaquePointer?
             
-//         }
-//         else{
-//             print("enter fname and lname")
-//         }
+            //             var sShowData = ""
+            
+            //            if sqlite3_prepare_v2(dbQueue, selectStatementString, -1, &selectStatemetnQuery, nil) == SQLITE_OK
+            //             {
+            //                 while sqlite3_step(selectStatemetnQuery) == SQLITE_ROW {
+            
+            //                     sShowData += String(cString: sqlite3_column_text(selectStatemetnQuery, 0)) + "\t\t" +
+            //                                  String(cString: sqlite3_column_text(selectStatemetnQuery, 1)) + "\t\t" +
+            //                                  String(cString: sqlite3_column_text(selectStatemetnQuery, 2)) + "\t\t" +
+            //                                 String(cString: sqlite3_column_text(selectStatemetnQuery, 3)) + "\t\t" +
+            //                                 String(cString: sqlite3_column_text(selectStatemetnQuery, 4)) + "\t\t" +
+            //                                 String(cString: sqlite3_column_text(selectStatemetnQuery, 5)) + "\n"
+            //                     }
+            
+            //              //  sShowData += String(cString: sqlit)
+            
+            //                 sqlite3_finalize(selectStatemetnQuery)
+            //             }
+            //             print("selecting data")
+            //             print(sShowData)
+            
+            //         }
+            //         else{
+            //             print("enter fname and lname")
+            //         }
+            
+        }
         
     }
-
+    
 }
-
