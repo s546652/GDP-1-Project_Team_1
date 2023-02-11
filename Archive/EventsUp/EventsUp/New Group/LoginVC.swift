5cdd3798554264369ef059dc342d340ebbd4d31f
Rajitha Bandaru <48714015+Rajitha9595@users.noreply.github.com>
2023-02-10 21:18:03 -0600

    Add alert for login button
    Alert message is required after a successful login

--- Archive/EventsUp/EventsUp/New Group/LoginVC.swift
+++ Archive/EventsUp/EventsUp/New Group/LoginVC.swift
@@ -10,6 +10,24 @@ import Firebase
 
 class LoginVC: UIViewController {
 
+    @IBAction func LoginBTN(_ sender: UIButton) {
+        // Create new Alert
+        var dialogMessage = UIAlertController(title: "Success", message: "You are succesfully logged in", preferredStyle: .alert)
+        
+        // Create OK button with action handler
+        let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+            print("Ok button tapped")
+         })
+        
+        //Add OK button to a dialog message
+        dialogMessage.addAction(ok)
+
+        // Present Alert to
+        self.present(dialogMessage, animated: true, completion: nil)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
