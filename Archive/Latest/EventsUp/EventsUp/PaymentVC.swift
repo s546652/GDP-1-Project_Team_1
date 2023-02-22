d8bcdc070eb19f0f887302075724fd1c0301aab9
naveen007kumar <30547716+naveen007kumar@users.noreply.github.com>
2023-02-21 22:01:04 -0600

    Add User profile and payment View Controllers
    User profile and Payment View Controllers are added and swift files with respective to these controllers are added

---
+++ Archive/Latest/EventsUp/EventsUp/PaymentVC.swift
@@ -0,0 +1,29 @@
+//
+//  PaymentVC.swift
+//  EventsUp
+//
+//  Created by Gundu,Naveen Kumar on 2/21/23.
+//
+
+import UIKit
+
+class PaymentVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
