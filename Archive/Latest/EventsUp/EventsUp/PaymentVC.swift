4dcccfad5ebd39898422180ffcd5b36c3222c570
naveen007kumar <30547716+naveen007kumar@users.noreply.github.com>
2023-02-22 09:51:04 -0600

    Add animation in Payment page
    Lottie Animation is developed in the payment page in the background of the page

--- Archive/Latest/EventsUp/EventsUp/PaymentVC.swift
+++ Archive/Latest/EventsUp/EventsUp/PaymentVC.swift
@@ -6,16 +6,32 @@
 //
 
 import UIKit
+import Lottie
 
 class PaymentVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        let animationView = LottieAnimationView()
+                let animation = LottieAnimation.named("31737-savings-dollar")
+                animationView.animation = animation
+                animationView.loopMode = .loop
+                animationView.frame = view.bounds
+        animationView.alpha = 0.7
+
+                view.layer.addSublayer(animationView.layer)
+                animationView.play()
         // Do any additional setup after loading the view.
     }
     
 
+    @IBAction func PaypalBTN(_ sender: UIButton) {
+        if let url = URL(string: "https://paypal.me/Naveen296") {
+                     UIApplication.shared.open(url)
+                 }
+    }
     /*
     // MARK: - Navigation
 
