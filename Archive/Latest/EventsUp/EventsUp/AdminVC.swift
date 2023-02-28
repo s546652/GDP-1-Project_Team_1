8e9619d4ea3156a9b54f0598700d41622c6da825
naveen007kumar <30547716+naveen007kumar@users.noreply.github.com>
2023-02-28 17:01:30 -0600

    Add animation in new page
    A new login animation is added in admin login page

--- Archive/Latest/EventsUp/EventsUp/AdminVC.swift
+++ Archive/Latest/EventsUp/EventsUp/AdminVC.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import Lottie
 
 class AdminVC: UIViewController {
 
+    
+    @IBOutlet weak var LoginIV: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+                let animationView = LottieAnimationView()
+                LoginIV.addSubview(animationView)
+
+                let animation = LottieAnimation.named("login")
+                animationView.animation = animation
+                animationView.loopMode = .loop
+                animationView.frame = CGRect(x: 50, y: 120, width: 300, height: 300)
+               // animationView.center = view.center
+
+        view.layer.addSublayer(animationView.layer)
+        animationView.play()
         // Do any additional setup after loading the view.
     }
     
