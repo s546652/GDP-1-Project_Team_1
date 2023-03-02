b91d63a636134ecdba5f10aaea9b74a963bfa047
rgagenapally <120142485+rgagenapally@users.noreply.github.com>
2023-03-01 20:05:41 -0600

    Admin Detail
    Adding Admin page.

---
+++ Archive/Latest/EventsUp/EventsUp/AdminDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  AdminDetailVC.swift
+//  EventsUp
+//
+//  Created by Gagenapally,Roshni Damodar Reddy on 3/1/23.
+//
+
+import UIKit
+
+class AdminDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
